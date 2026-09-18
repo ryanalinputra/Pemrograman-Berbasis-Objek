80c74bafa6903cd28ccc809db9d5bfe3c5f231e7
ryanalinputraa <ryanalinputraa@email.com>
2026-09-18 10:49:13 +0700

    Praktikum 3

--- Praktikum 3/kalkulator/main.py
+++ Praktikum 3/kalkulator/main.py
@@ -8,8 +8,8 @@ def konfirmasi():
 print("=== KALKULATOR ===")
 while True:
     try:
-        a = int(input("Masukkan angka pertama: "))
-        b = int(input("Masukkan angka kedua: "))
+        a = float(input("Masukkan angka pertama: "))
+        b = float(input("Masukkan angka kedua: "))
     except ValueError:
         print("Error: Input harus berupa angka")
         konfirmasi()
@@ -18,7 +18,7 @@ while True:
         print("2. Kurang")
         print("3. Kali")
         print("4. Bagi")
-        pilihan = float(input("Masukkan pilihan: "))
+        pilihan = int(input("Masukkan pilihan: "))
         if pilihan == 1:
             print("Hasil: ", kalkulator.tambah(a, b))
             konfirmasi()
